b10de12124a2b9aa88200edc927efd1b02d6d3ff
Ismael Serratos <ismael@wizeline.com>
2015-12-29 20:32:12 -0600

    End of chap 2

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('./main.css')
+
 var component = require('./component');
 var app = document.createElement('div');
 
--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,19 @@ const common = {
         new HtmlwebpackPlugin({
             title: 'Kanban App'
         })
-    ]
+    ],
+    module: {
+      loaders: [{
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }]
+    }
 }
 
 if (TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
+    devtool: 'eval-source-map',
     devServer: {
       historyApiFallback: true,
       hot: true,
